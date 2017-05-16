b8f61113ee6a5273986aeea1f2d7d1103824d531
Brice Argenson <bargenson@gmail.com>
2017-05-16 19:14:38 -0400

    Remove junit reporter

--- karma.conf.js
+++ karma.conf.js
@@ -30,12 +30,7 @@ module.exports = function(config) {
 
     // test results reporter to use
     // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-    reporters: ['spec', 'junit'],
-
-    junitReporter: {
-      outputDir: 'tests-results', // results will be saved as $outputDir/$browserName.xml
-      useBrowserName: true // add browser name to report and classes names
-    },
+    reporters: ['spec'],
 
     // web server port
     port: 8080,
